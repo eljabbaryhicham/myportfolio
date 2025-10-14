5b0bdde1898752c5045a1b3ae9a251475d017424
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-14 20:12:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8316917408-a299a",
+  "appId": "1:844227900797:web:108ac82ca89bdc851d4814",
+  "apiKey": "AIzaSyDv1Bz1x0TlqEQpp-SFjSKtDlb0te8E214",
+  "authDomain": "studio-8316917408-a299a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844227900797"
+};
