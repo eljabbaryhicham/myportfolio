3882c3a66ca7f841bb320886578a8e6e986213af
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-16 04:26:35 +0000

    the popup section miminum height should be 90%, and add close button on

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -164,7 +164,7 @@ export default function WorkPage() {
       </div>
 
       <Dialog open={!!selectedItem} onOpenChange={handleOpenChange}>
-        <DialogContent className="w-[90vw] md:max-w-[80vw] h-auto glass-effect p-0 flex flex-col">
+        <DialogContent className="w-[90vw] md:max-w-[80vw] min-h-[90vh] glass-effect p-0 flex flex-col">
           {selectedItem && (
             <div className="relative flex-1 flex flex-col min-h-0">
               <ScrollArea className="flex-1">
