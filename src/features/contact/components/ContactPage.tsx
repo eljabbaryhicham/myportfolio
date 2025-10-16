31e8ce9dbd40cadc9ba249c23bd18e4e52812f24
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-16 17:31:35 +0000

    in the contact page, please force the cards sections to be smaller like

--- src/features/contact/components/ContactPage.tsx
+++ src/features/contact/components/ContactPage.tsx
@@ -95,7 +95,7 @@ export default function ContactPage() {
         <div className="p-[5%] pt-0">
           <div className="container mx-auto px-0">
             <div className="flex flex-col md:flex-row gap-8 items-center justify-center">
-              <div className="md:w-1/2 w-full m-[5%]">
+              <div className="md:w-[30%] w-full m-[5%]">
                 <Card className="glass-effect p-6 sm:p-8 h-full">
                   <CardContent className="p-0">
                     <Form {...form}>
@@ -158,7 +158,7 @@ export default function ContactPage() {
                 </Card>
               </div>
               
-              <div className="md:w-1/2 w-full m-[5%]">
+              <div className="md:w-[30%] w-full m-[5%]">
                 <Card className="glass-effect p-6 flex flex-col h-full">
                   <CardContent className="flex flex-col items-center text-center p-0">
                     <Avatar className="w-24 h-24 mb-4 border-2 border-primary">
