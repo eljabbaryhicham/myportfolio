3381bd13616d003b1d25f95b60ca5e0469ed2b1f
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-16 06:25:28 +0000

    in mobile version the navigation menu height should be fit its content w

--- src/components/layout/app-nav.tsx
+++ src/components/layout/app-nav.tsx
@@ -20,7 +20,7 @@ export function AppNav() {
 
   return (
     <aside className="w-full md:w-28 flex-shrink-0 p-0 md:p-4 mt-4 md:mt-0">
-      <div className={cn("flex h-full flex-row md:flex-col items-center justify-between rounded-lg border border-border/50 p-8 glass-effect")}>
+      <div className={cn("flex h-auto md:h-full flex-row md:flex-col items-center justify-between rounded-lg border border-border/50 py-[10%] px-8 md:p-8 glass-effect")}>
         <Link href="/" className="flex items-center gap-2 text-primary">
           <Cat className="h-8 w-8" />
         </Link>
