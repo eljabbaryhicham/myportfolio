b079c808d8ed26279cc41719ca9add3b0d6cea3a
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-16 06:13:48 +0000

    the details section height should be always 90% of the page height

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -230,7 +230,7 @@ export default function WorkPage() {
       
       {/* Nested Dialog for Details */}
       <Dialog open={detailsModalOpen} onOpenChange={setDetailsModalOpen}>
-        <DialogContent className="w-[90vw] md:max-w-[80vw] h-auto md:h-[90vh] glass-effect p-0 flex flex-col">
+        <DialogContent className="w-[90vw] md:max-w-[80vw] h-[90vh] glass-effect p-0 flex flex-col">
             {selectedItem && (
                 <>
                 <DialogHeader className="p-6 pb-0">
