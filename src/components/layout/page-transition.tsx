9b0dace1d2434e2508316f39c183e0f1d4eae6bb
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-16 20:10:00 +0000

    remove the fade transition

--- src/components/layout/page-transition.tsx
+++ src/components/layout/page-transition.tsx
@@ -1,46 +1,12 @@
 
 'use client';
 
-import { usePathname } from 'next/navigation';
-import { AnimatePresence, motion } from 'framer-motion';
-
-const variants = {
-  initial: {
-    opacity: 0,
-  },
-  enter: {
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-      ease: [0.33, 1, 0.68, 1],
-    },
-  },
-  exit: {
-    opacity: 0,
-    transition: {
-      duration: 0.5,
-      ease: [0.33, 1, 0.68, 1],
-    },
-  },
-};
+import React from 'react';
 
 export function PageTransition({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
   return (
-    <>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={pathname}
-          variants={variants}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          className="h-full w-full"
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
-    </>
+    <div className="h-full w-full">
+      {children}
+    </div>
   );
 }
