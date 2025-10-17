101425166a0a8077686a1144abbac8a876207fa6
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-17 16:29:43 +0000

    don't change anything else except replace all red colors on the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
       </head>
       <body className={cn('font-body antialiased text-center')} suppressHydrationWarning>
         <div className="fixed inset-0 -z-10 overflow-hidden" style={{ filter: 'url(#metaballs)' }}>
-          <div className="absolute w-[60rem] h-[60rem] bg-red-500/30 rounded-full blur-3xl opacity-40 top-1/4 left-1/4 blob-1"></div>
-          <div className="absolute w-[50rem] h-[50rem] bg-red-500/30 rounded-full blur-3xl opacity-40 bottom-1/4 right-1/4 blob-2"></div>
-          <div className="absolute w-[40rem] h-[40rem] bg-red-500/30 rounded-full blur-3xl opacity-40 top-1/2 left-1/2 blob-3"></div>
+          <div className="absolute w-[60rem] h-[60rem] bg-primary/30 rounded-full blur-3xl opacity-40 top-1/4 left-1/4 blob-1"></div>
+          <div className="absolute w-[50rem] h-[50rem] bg-primary/30 rounded-full blur-3xl opacity-40 bottom-1/4 right-1/4 blob-2"></div>
+          <div className="absolute w-[40rem] h-[40rem] bg-primary/30 rounded-full blur-3xl opacity-40 top-1/2 left-1/2 blob-3"></div>
         </div>
         <svg className="absolute w-0 h-0">
           <defs>
