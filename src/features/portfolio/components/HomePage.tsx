65f97280799a22f0d0cbff42329f22eac9b694c6
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-18 22:54:14 +0000

    still get the same issue

--- src/features/portfolio/components/HomePage.tsx
+++ src/features/portfolio/components/HomePage.tsx
@@ -1,9 +1,45 @@
 
+'use client';
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import HomePageVideo from "./HomePageVideo";
+import { H5Player } from 'h5player';
+import { useEffect, useState } from 'react';
+
+const HomePageVideo = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return null; // Don't render on the server
+    }
+
+    return (
+        <H5Player
+            source="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+            className="w-full h-full"
+            config={{
+                isLive: false,
+                autoplay: true,
+                muted: true,
+                loop: true,
+                fluid: true,
+                'x5-video-player-fullscreen': false,
+                'x5-playsinline': true,
+                playsinline: true,
+                'x-webkit-airplay': false,
+                'airplay-fullscreen': false,
+                controls: false, // hide controls
+                ignores: ['error', 'volume', 'playbackrate', 'play'],
+            }}
+        />
+    );
+};
 
 export default function HomePage() {
   return (
