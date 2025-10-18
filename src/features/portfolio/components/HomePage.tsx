60effffa778aae9d604471132c1897b76031e9c5
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-18 23:04:55 +0000

    make the video container fit the video

--- src/features/portfolio/components/HomePage.tsx
+++ src/features/portfolio/components/HomePage.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { defaultPortfolioItems } from "../data/portfolio-data";
 import H5Player from "./H5Player";
+import { cn } from "@/lib/utils";
 
 export default function HomePage() {
   const featuredVideo = defaultPortfolioItems.find(item => item.type === 'video' && item.featured);
@@ -16,8 +17,11 @@ export default function HomePage() {
     || 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4';
 
   return (
-    <div className="h-full w-full flex flex-col items-center justify-center gap-8">
-      <div className="w-full h-1/2 relative rounded-lg overflow-hidden glass-effect border border-border/50">
+    <div className="h-full w-full flex flex-col items-center justify-center gap-8 p-4">
+      <div className={cn(
+        "w-full max-w-4xl aspect-video", // Use aspect-video for 16:9 ratio
+        "relative rounded-lg overflow-hidden glass-effect border border-border/50"
+      )}>
         <H5Player source={videoSrc} />
       </div>
       <Button asChild size="lg" className="group">
