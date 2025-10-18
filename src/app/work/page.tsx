da9b2a7507252885ea3604ac6981c52491372d42
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-18 06:28:46 +0000

    832f217 version is good, only thing is make the dialog bigger while keep

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -335,7 +335,7 @@ export default function WorkPage() {
       <Dialog open={!!selectedItem} onOpenChange={handleOpenChange}>
         <DialogContent 
             className={cn(
-                "w-[90vw] max-w-[80vh] max-h-[90vh] glass-effect p-0 flex flex-col"
+                "aspect-video w-[95vw] max-w-[140vh] glass-effect p-0 flex flex-col"
             )}
         >
           {selectedItem && (
@@ -512,5 +512,7 @@ export default function WorkPage() {
 
 
 
+
+    
 
     
