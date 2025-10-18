f36977f6509ff86d9c70bc9709d0277af12b7b36
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-18 22:48:58 +0000

    in home page add a video 50% size of the screen above the button "explor

--- src/features/portfolio/components/HomePage.tsx
+++ src/features/portfolio/components/HomePage.tsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import HomePageVideo from "./HomePageVideo";
 
 export default function HomePage() {
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className="h-full w-full flex flex-col items-center justify-center gap-8">
+      <div className="w-full h-1/2 relative rounded-lg overflow-hidden glass-effect border border-border/50">
+        <HomePageVideo />
+      </div>
       <Button asChild size="lg" className="group">
         <Link href="/work">
           Explore Work
