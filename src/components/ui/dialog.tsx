ff89182744fead970c5c974d264e10a5abb20c1f
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-19 14:12:47 +0000

    great jus the project dialog is not centered in the page !!

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Slot } from "@radix-ui/react-slot";
+import { motion } from "framer-motion";
 
 const Dialog = DialogPrimitive.Root
 
@@ -35,21 +36,20 @@ const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & { asChild?: boolean }
 >(({ className, children, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "div";
+    const Comp = asChild ? motion.div : DialogPrimitive.Content;
     return (
       <DialogPortal>
         <DialogOverlay />
-        <DialogPrimitive.Content
+        <Comp
           ref={ref}
           className={cn(
             "fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
             className
           )}
           {...props}
-          asChild
         >
-          <Comp>{children}</Comp>
-        </DialogPrimitive.Content>
+          {children}
+        </Comp>
       </DialogPortal>
     );
 });
@@ -122,5 +122,3 @@ export {
   DialogTitle,
   DialogDescription,
 }
-
-    
