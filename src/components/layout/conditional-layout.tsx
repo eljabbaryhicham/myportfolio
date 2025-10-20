f641980e3622a83b5e560e058e290a137bbd9907
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-20 12:20:05 +0000

    in small height screen there's no space between the main content and the

--- src/components/layout/conditional-layout.tsx
+++ src/components/layout/conditional-layout.tsx
@@ -32,7 +32,7 @@ export function ConditionalLayout({ children }: { children: React.ReactNode }) {
   return (
     <AnimatePresence>
       <motion.div
-        className="flex h-full flex-col md:flex-row p-2 md:p-4 pb-[calc(7vh+1rem)] md:pb-4"
+        className="flex h-full flex-col md:flex-row p-2 md:p-4 pb-[calc(60px+2rem)] md:pb-4"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
