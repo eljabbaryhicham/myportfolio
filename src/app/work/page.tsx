86b9b30d9d8c487c131cdc90c6678c58535fb317
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-20 11:43:16 +0000

    no just increase Grid Gap on mobile version

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -505,7 +505,7 @@ export default function WorkPage() {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.3 }}
-                    className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4"
+                    className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
                   >
                     {filteredItems.slice(0, visibleItems).map(item => (
                       <PortfolioGridItem 
