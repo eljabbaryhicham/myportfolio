00ebdda2c3c6beb7df41df143f8daec38bfcab38
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-21 23:46:11 +0000

    let's try this .masonry {   display: grid;   grid-template-columns: repe

--- src/features/about/components/AboutPage.tsx
+++ src/features/about/components/AboutPage.tsx
@@ -166,7 +166,7 @@ export default function AboutPage() {
                     </div>
                     <div className="md:w-1/2">
                        <h2 className="text-2xl md:text-3xl font-headline tracking-tight mb-6 text-center">What We Do?</h2>
-                       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                       <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4">
                         {services.map((service) => (
                             <div 
                                 key={service.title}
