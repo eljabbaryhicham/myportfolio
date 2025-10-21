26a03fb7731b1f63bc47803cff46f0c1c9537fbd
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-21 14:48:20 +0000

    make the main website font is "Nunito" from google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Nunito', 'sans-serif'],
+        headline: ['Nunito', 'sans-serif'],
         code: ['monospace'],
         handwriting: ['Dancing Script', 'cursive'],
       },
