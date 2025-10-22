4b5136dd2e4b03882a61dd44395c8d67e4e7fee6
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-22 06:12:15 +0000

    remove the download button and also remove the default shaka player prel

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -40,62 +40,11 @@ const VideoPlayer = ({
     const initPlayer = async () => {
         const shaka = await import('shaka-player/dist/shaka-player.ui');
 
-        // Define the custom button class AFTER shaka.ui is loaded.
-        class DownloadButton extends shaka.ui.Element {
-          private button_: HTMLButtonElement;
-
-          constructor(parent: HTMLElement, controls: shaka.ui.Controls) {
-            super(parent, controls);
-
-            this.button_ = document.createElement('button');
-            this.button_.className = 'shaka-download-button shaka-control-button';
-            this.button_.innerHTML = '<i class="material-icons">download</i>'; // Using Material Icons font
-            this.button_.setAttribute('aria-label', 'Download');
-            this.parent.appendChild(this.button_);
-
-            this.eventManager.listen(this.button_, 'click', () => {
-              this.onDownloadClick_();
-            });
-          }
-
-          private onDownloadClick_() {
-            const assetUri = this.player.getAssetUri();
-            if (assetUri) {
-              const link = document.createElement('a');
-              link.href = assetUri;
-              
-              try {
-                const url = new URL(assetUri);
-                link.download = url.pathname.split('/').pop() || 'video';
-              } catch (e) {
-                link.download = 'video';
-              }
-              
-              document.body.appendChild(link);
-              link.click();
-              document.body.removeChild(link);
-            }
-          }
-
-          release() {
-            super.release();
-          }
-        }
-
-        DownloadButton.Factory = class {
-          create(rootElement: HTMLElement, controls: shaka.ui.Controls) {
-            return new DownloadButton(rootElement, controls);
-          }
-        };
-
         shaka.polyfill.installAll();
         if (!shaka.Player.isBrowserSupported()) {
             console.error('Browser not supported!');
             return;
         }
-
-        // Register the custom button. It's safe to call this multiple times.
-        shaka.ui.Controls.registerElement('download', new DownloadButton.Factory());
         
         player = new shaka.Player(videoRef.current);
 
@@ -127,7 +76,6 @@ const VideoPlayer = ({
                     'spacer',
                     'volume',
                     'fullscreen',
-                    'download', // Add the download button here
                     'overflow_menu',
                 ],
                 overflowMenuButtons: ['quality', 'picture_in_picture', 'loop', 'captions', 'playback_rate'],
