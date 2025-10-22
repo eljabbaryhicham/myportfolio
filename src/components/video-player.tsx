fd4f8a208d100563b058e9e46f36a6a51777a64e
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-22 06:09:44 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -6,57 +6,6 @@ import { cn } from '@/lib/utils';
 import 'shaka-player/dist/controls.css';
 import Preloader from './preloader';
 
-// Custom Download Button for Shaka Player
-class DownloadButton extends window.shaka.ui.Element {
-  private button_: HTMLButtonElement;
-
-  constructor(parent: HTMLElement, controls: shaka.ui.Controls) {
-    super(parent, controls);
-
-    this.button_ = document.createElement('button');
-    this.button_.className = 'shaka-download-button shaka-control-button';
-    this.button_.innerHTML = '<i class="material-icons">download</i>'; // Using Material Icons font
-    this.button_.setAttribute('aria-label', 'Download');
-    this.parent.appendChild(this.button_);
-
-    this.eventManager.listen(this.button_, 'click', () => {
-      this.onDownloadClick_();
-    });
-  }
-
-  private onDownloadClick_() {
-    const assetUri = this.player.getAssetUri();
-    if (assetUri) {
-      const link = document.createElement('a');
-      link.href = assetUri;
-      
-      // Try to get a nice filename
-      try {
-        const url = new URL(assetUri);
-        link.download = url.pathname.split('/').pop() || 'video';
-      } catch (e) {
-        link.download = 'video';
-      }
-      
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }
-
-  // This is required by the shaka.extern.IUIElement interface.
-  release() {
-    super.release();
-  }
-}
-
-DownloadButton.Factory = class {
-  create(rootElement: HTMLElement, controls: shaka.ui.Controls) {
-    return new DownloadButton(rootElement, controls);
-  }
-};
-
-
 interface VideoPlayerProps {
   src?: string;
   poster?: string;
@@ -90,6 +39,55 @@ const VideoPlayer = ({
 
     const initPlayer = async () => {
         const shaka = await import('shaka-player/dist/shaka-player.ui');
+
+        // Define the custom button class AFTER shaka.ui is loaded.
+        class DownloadButton extends shaka.ui.Element {
+          private button_: HTMLButtonElement;
+
+          constructor(parent: HTMLElement, controls: shaka.ui.Controls) {
+            super(parent, controls);
+
+            this.button_ = document.createElement('button');
+            this.button_.className = 'shaka-download-button shaka-control-button';
+            this.button_.innerHTML = '<i class="material-icons">download</i>'; // Using Material Icons font
+            this.button_.setAttribute('aria-label', 'Download');
+            this.parent.appendChild(this.button_);
+
+            this.eventManager.listen(this.button_, 'click', () => {
+              this.onDownloadClick_();
+            });
+          }
+
+          private onDownloadClick_() {
+            const assetUri = this.player.getAssetUri();
+            if (assetUri) {
+              const link = document.createElement('a');
+              link.href = assetUri;
+              
+              try {
+                const url = new URL(assetUri);
+                link.download = url.pathname.split('/').pop() || 'video';
+              } catch (e) {
+                link.download = 'video';
+              }
+              
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
+            }
+          }
+
+          release() {
+            super.release();
+          }
+        }
+
+        DownloadButton.Factory = class {
+          create(rootElement: HTMLElement, controls: shaka.ui.Controls) {
+            return new DownloadButton(rootElement, controls);
+          }
+        };
+
         shaka.polyfill.installAll();
         if (!shaka.Player.isBrowserSupported()) {
             console.error('Browser not supported!');
