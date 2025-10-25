e6a511c81cbafcec5ea1ab22c6698948e4281c02
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-25 17:06:37 +0000

    plyr still not pause !!

--- src/components/PlyrPlayer.tsx
+++ src/components/PlyrPlayer.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
+import React, { useEffect, useRef, forwardRef } from 'react';
 import Plyr from 'plyr-react';
 import 'plyr/dist/plyr.css';
 import Hls from 'hls.js';
@@ -12,14 +12,11 @@ interface PlyrPlayerProps {
   watermark?: string;
 }
 
-const PlyrPlayer = forwardRef(({ source, poster, watermark }: PlyrPlayerProps, ref) => {
-  const internalRef = useRef<any>(null);
-  
-  // Expose the internal Plyr player instance to the parent component.
-  useImperativeHandle(ref, () => internalRef.current?.plyr);
+const PlyrPlayer = forwardRef<any, PlyrPlayerProps>(({ source, poster, watermark }, ref) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    const videoElement = internalRef.current?.media;
+    const videoElement = videoRef.current;
     if (!videoElement) return;
 
     let hls: Hls | null = null;
@@ -48,8 +45,7 @@ const PlyrPlayer = forwardRef(({ source, poster, watermark }: PlyrPlayerProps, r
     playsinline: true,
     clickToPlay: true,
   };
-
-  // We set a simple source object here because Hls.js will handle the actual streaming.
+  
   const plyrSource = {
     type: 'video' as 'video',
     poster: poster,
@@ -99,11 +95,8 @@ const PlyrPlayer = forwardRef(({ source, poster, watermark }: PlyrPlayerProps, r
         `}
       </style>
       <div className="relative w-full h-full">
-        {/*
-          The `ref` here is now the `internalRef`. `useImperativeHandle` connects this
-          to the `ref` from the parent component.
-        */}
-        <Plyr ref={internalRef} source={plyrSource} options={plyrOptions} />
+        {/* Pass the ref directly to the Plyr component */}
+        <Plyr ref={ref} source={plyrSource} options={plyrOptions} />
         {watermark && (
             <div className="plyr__watermark">
                 <img src={watermark} alt="Watermark" />
