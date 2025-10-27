7976c9b3a440fc2e972eb430473cf422c59d5148
DIGITAL ART <eljabaryhicham@gmail.com>
2025-10-27 02:56:17 +0000

    Let's force the android mobile use GPU acceleration to get the plyr play

--- src/components/PlyrPlayer.tsx
+++ src/components/PlyrPlayer.tsx
@@ -273,8 +273,10 @@ const PlyrPlayer = forwardRef(({ source, poster, watermark, autoPlay = true, thu
     }
   }, [autoPlay, isLoading]); // Re-run when isLoading changes to ensure play is called after ready
 
+  const forceGpuClass = isMobile && /Android/i.test(navigator.userAgent) ? 'force-gpu' : '';
+
   return (
-    <div className="relative w-full h-full bg-black">
+    <div className={cn("relative w-full h-full bg-black", forceGpuClass)}>
       <style>
         {`
           :root {
